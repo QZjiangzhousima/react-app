af0140eba5674a8746277d80c6d4bcfd4bd45796
Your Name <1025335609@qq.com>
2020-07-08 14:37:06 +0800

    第一次提交

---
+++ src/pages/Edu/Test/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render () {
+        return (
+            <div>
+                Test测试....hello word
+                <div>
+                </div></div>
+        )
+    }
+}
